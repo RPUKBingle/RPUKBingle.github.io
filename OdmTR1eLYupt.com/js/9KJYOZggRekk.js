2ac7149c4b294b28d45687df72baae553da68f91
Leah kemp <tocoman5000@gmail.com>
2024-01-02 07:12:48 +0000

    first commit

---
+++ OdmTR1eLYupt.com/js/9KJYOZggRekk.js
@@ -0,0 +1,80 @@
+var youtube = "https://www.youtube.com/@RPUK-Bingle";
+var twitter = " ";
+var password = "Binglebongle";
+var linkedin = " ";
+var instagram = " ";
+var github = " ";
+var email = ' ';
+
+pastleaks = [
+  "<br>",
+  "</span> <span class=\"command\">CID Handbook</span><span class=\"color2\">               https://docs.google.com/document/d/1T2CUiLRL9NNVw0vC8a78b8zeurAQYpO6iHKXBULlyp8/edit#heading=h.6brwnvbxlu3n</span>",
+  "</span> <span class=\"command\">SMS Records Example</span><span class=\"color2\">        https://docs.google.com/spreadsheets/d/1ussY6rLgBPjEOv9VNlVbB_TIqzEtwsMLx3vxIWtfVn0/edit#gid=62415168</span>",
+  "</span> <span class=\"command\">Operation Lifeless Dog</span><span class=\"color2\">     https://docs.google.com/document/d/1St55X7LTpey6c3iVKO0NJ9g8EPHJG8wIgWLpn7ZOr8E/edit?usp=sharing</span>",
+  "</span> <span class=\"command\">VSR | Jerry Johnson</span><span class=\"color2\">        https://docs.google.com/document/d/1-aNLXT8X__iiEE6SJArXJXynzDDCFhciPlIylfBQ-RU/edit</span>",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+];
+
+social = [
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "                                                                   ██████╗ ██╗███╗   ██╗ ██████╗ ██╗     ███████╗",
+  "                                                                   ██╔══██╗██║████╗  ██║██╔════╝ ██║     ██╔════╝",
+  "                                                                   ██████╔╝██║██╔██╗ ██║██║  ███╗██║     █████╗  ",
+  "                                                                   ██╔══██╗██║██║╚██╗██║██║   ██║██║     ██╔══╝  ",
+  "                                                                   ██████╔╝██║██║ ╚████║╚██████╔╝███████╗███████╗",
+  "                                                                   ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚══════╝",
+  "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------",
+  "   Bingle is not your typical organization. They're not a political party, a corporation, or an NGO. They're a rebellion, a voice of the people who are tired of being silenced, ignored,",
+  "         and oppressed. Bingle is made up of ordinary citizens, people like you and me, who have had enough of the government's lies, corruption, and abuse of power. Bingle believes",
+  "     in the power of community, in the strength of unity, and in the courage of those who stand up against injustice. They're not afraid to speak truth to power, to take risks, and to",
+  "                        fight for what they believe in. Bingle is a movement, a force for change, and a symbol of hope for all those who seek a better future.",
+  "    Bingle's motives revolve around challenging the oppressive actions of the government and fighting for a better society. The organization believes that the government has",
+  "       overstepped its bounds and is no longer serving the best interests of the people. Bingle is driven by the belief that change is necessary and that the status quo is",
+  "   unacceptable. They aim to inspire others to take action and join them in the fight for a brighter future. Bingle sees itself as a beacon of hope, a voice for the voiceless, and a",
+  "                                                         powerful force for change in a world that desperately needs it.",
+];
+
+help = [
+  "<br>",
+  '<span class="command">pastleaks</span>      A List of our past Leaks',
+  '<span class="command">who_are_we</span>       Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">members_portal</span> Bingle Members Portal Login Page',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+Dir = [
+  "<br>",
+  "</span> <span class=\"command\">VSR | Jerry Johnson</span><span class=\"color2\">",
+];
+
+members_portal = [
+  "<br>",
+  '<span class="command">cid database</span>      A backdoor into the CID database',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">OOC: Please do not data mine this website <3</span>',
+  "          (it just spoils the fun)",
+  "██████╗ ██╗███╗   ██╗ ██████╗ ██╗     ███████╗",
+  "██╔══██╗██║████╗  ██║██╔════╝ ██║     ██╔════╝",
+  "██████╔╝██║██╔██╗ ██║██║  ███╗██║     █████╗  ",
+  "██╔══██╗██║██║╚██╗██║██║   ██║██║     ██╔══╝  ",
+  "██████╔╝██║██║ ╚████║╚██████╔╝███████╗███████╗",
+  "╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚══════╝",
+  "///        Website Hosted in Zürich        ///",
+  '<span class="color2">Welcome to the B.I.T (Bingle Interactive Terminal)</span>',
+  "<span class=\"color2\">To check The directory please, type</span> <span class=\"command\">'dir'</span><span class=\"color2\">.</span>",
+];
